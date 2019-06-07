e9ed140d9911c1b4e1b31328390f737b0aace946
hungts1998 <hungts510@gmail.com>
2019-06-07 22:47:41 +0700

    roll back origin version

--- src/main/java/hieuhung/spring/Application.java
+++ src/main/java/hieuhung/spring/Application.java
@@ -2,13 +2,8 @@ package hieuhung.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("org.baeldung.persistence.repo")
-@EntityScan("org.baeldung.persistence.model")
-
-@SpringBootApplication(scanBasePackages={"hieuhung.spring.model"})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/hieuhung/spring/controller/loginController.java
+++ src/main/java/hieuhung/spring/controller/loginController.java
@@ -1,7 +1,5 @@
 package hieuhung.spring.controller;
 
-import hieuhung.spring.model.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class loginController {
 
-    @Autowired
-    private UserRepo userRepo;
 
     @GetMapping("/login")
     public String loginInit(Model model) {

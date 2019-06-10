54f58f41b018029ba96007816926d34de077e26b
khauthanhhieu <thanhhieu021298@gmail.com>
2019-06-10 21:05:14 +0700

    option to hide login and logout buttons; Display username when logged in

--- src/main/java/hieuhung/spring/controller/HomeController.java
+++ src/main/java/hieuhung/spring/controller/HomeController.java
@@ -3,6 +3,8 @@ package hieuhung.spring.controller;
 import hieuhung.spring.model.Eatery;
 import hieuhung.spring.service.EateryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +20,10 @@ public class HomeController {
     public String main(Model model) {
 
         List<Eatery> eateryList = eateryService.getAllEatery();
-
         model.addAttribute("eateryList", eateryList);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth.getName() + " - " + auth.getAuthorities());
 
         return "index"; //view
     }
